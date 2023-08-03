1f8d51506ddd5dcfaaf9edafd7efeff1bcba7349
Rehan Zuberi <rehanzuberi@rehans-air.home>
2023-08-03 12:52:22 +0200

    Added the rotation augmentation in data_utils.py, put the augmentation processes into their own function

--- data_utils.py
+++ data_utils.py
@@ -181,6 +181,89 @@ def adjust_brightness(image, brightness_factor):
     # adjusted_image = torch.clamp(adjusted_image, 0.0, 1.0)
     return adjusted_image
 
+def apply_rotation(original_image):
+    # Rotate by 90 degrees
+    rotated_90 = torch.rot90(original_image, k=1, dims=(1, 2))
+    # Rotate by 180 degrees
+    rotated_180 = torch.rot90(original_image, k=2, dims=(1, 2))
+    # Rotate by 270 degrees
+    rotated_270 = torch.rot90(original_image, k=3, dims=(1, 2))
+    return rotated_90, rotated_180, rotated_270
+
+
+def brightness_augmentation(tiled_images_final, intermediate_outputs_final, flows_and_cellprob_output_final):
+    brightness_min = 0.1
+    brightness_max = 3
+
+    new_tiled_images_final = []
+    new_intermediate_outputs_final = []
+    new_flows_and_cellprob_output_final = []
+
+    # Generate a list of random numbers that is the same length as range(tiled_images_final.shape[0])
+    np.random.seed(42)
+    random_numbers = np.random.uniform(0, 1, tiled_images_final.shape[0])
+
+    # Iterate through the tensors in tiled_images_final
+    for image_idx in range(tiled_images_final.shape[0]):
+        # Get the original image tensor from tiled_images_final
+        original_image = tiled_images_final[image_idx]
+
+        # Append the original tensor (without brightness changes) to the list
+        new_tiled_images_final.append(original_image)
+        new_intermediate_outputs_final.append(intermediate_outputs_final[image_idx])
+        new_flows_and_cellprob_output_final.append(flows_and_cellprob_output_final[image_idx])
+
+        # Apply random brightness change with a probability of 0.5
+        if random_numbers[image_idx] < 0.5:
+            brightness_factor = torch.tensor([torch.FloatTensor(1).uniform_(brightness_min, brightness_max)])
+            new_image = adjust_brightness(original_image, brightness_factor)
+
+            # Append the new tensor with brightness changes to the list
+            new_tiled_images_final.append(new_image)
+            new_intermediate_outputs_final.append(intermediate_outputs_final[image_idx])
+            new_flows_and_cellprob_output_final.append(flows_and_cellprob_output_final[image_idx])
+
+    # Convert the lists to PyTorch tensors
+    tiled_images_final = torch.stack(new_tiled_images_final)
+    intermediate_outputs_final = torch.stack(new_intermediate_outputs_final)
+    flows_and_cellprob_output_final = torch.stack(new_flows_and_cellprob_output_final)
+
+    return tiled_images_final, intermediate_outputs_final, flows_and_cellprob_output_final
+
+def rotation_augmentation(tiled_images_final, intermediate_outputs_final, flows_and_cellprob_output_final):
+    new_tiled_images_final = []
+    new_intermediate_outputs_final = []
+    new_flows_and_cellprob_output_final = []
+
+    # Generate a list of random numbers that is the same length as range(tiled_images_final.shape[0])
+    np.random.seed(43)
+    random_numbers = np.random.uniform(0, 1, tiled_images_final.shape[0])
+
+    # Iterate through the tensors in tiled_images_final
+    for image_idx in range(tiled_images_final.shape[0]):
+        # Get the original image tensor from tiled_images_final
+        original_image = tiled_images_final[image_idx]
+
+        new_tiled_images_final.append(original_image)
+        new_intermediate_outputs_final.append(intermediate_outputs_final[image_idx])
+        new_flows_and_cellprob_output_final.append(flows_and_cellprob_output_final[image_idx])
+
+        # Apply random brightness change with a probability of 0.5
+        if random_numbers[image_idx] < 0.5:
+            rotated_90, rotated_180, rotated_270 = apply_rotation(original_image)
+
+            # Append the rotated tensors to the list
+            new_tiled_images_final.extend([rotated_90, rotated_180, rotated_270])
+            new_intermediate_outputs_final.extend([intermediate_outputs_final[image_idx]] * 3)
+            new_flows_and_cellprob_output_final.extend([flows_and_cellprob_output_final[image_idx]] * 3)
+            
+    # Convert the lists to PyTorch tensors
+    tiled_images_final = torch.stack(new_tiled_images_final)
+    intermediate_outputs_final = torch.stack(new_intermediate_outputs_final)
+    flows_and_cellprob_output_final = torch.stack(new_flows_and_cellprob_output_final)
+
+    return tiled_images_final, intermediate_outputs_final, flows_and_cellprob_output_final
+
 def get_training_and_validation_loaders(cellpose_model_directory, image_folder, channel=None, augment=False):
     # Whole cellpose model
     segmentation_model = models.CellposeModel(gpu=True, model_type=cellpose_model_directory)
@@ -208,44 +291,13 @@ def get_training_and_validation_loaders(cellpose_model_directory, image_folder, 
     combined_images = combined_images[27:28]
 
     tiled_images_final, intermediate_outputs_final, flows_and_cellprob_output_final = get_data_cp_clean(cpnet, combined_images, rescale=rescale)
+    
 
     if augment == True:
-        #Brightness augmentation
-        brightness_min = 0.1
-        brightness_max = 3
-
-        new_tiled_images_final = []
-        new_intermediate_outputs_final = []
-        new_flows_and_cellprob_output_final = []
-
-        #generate a list of random numbers that is the same length as range(tiled_images_final.shape[0])
-        np.random.seed(42)
-        random_numbers = np.random.uniform(0, 1, tiled_images_final.shape[0])
-
-        # Iterate through the tensors in tiled_images_final
-        for image_idx in range(tiled_images_final.shape[0]):
-            # Get the original image tensor from tiled_images_final
-            original_image = tiled_images_final[image_idx]
-
-            # Append the original tensor (without brightness changes) to the list
-            new_tiled_images_final.append(original_image)
-            new_intermediate_outputs_final.append(intermediate_outputs_final[image_idx])
-            new_flows_and_cellprob_output_final.append(flows_and_cellprob_output_final[image_idx])
-
-            # Apply random brightness change with a probability of 0.5
-            if random_numbers[image_idx] < 0.5:
-                brightness_factor = torch.tensor([torch.FloatTensor(1).uniform_(brightness_min, brightness_max)])
-                new_image = adjust_brightness(original_image, brightness_factor)
-
-                # Append the new tensor with brightness changes to the list
-                new_tiled_images_final.append(new_image)
-                new_intermediate_outputs_final.append(intermediate_outputs_final[image_idx])
-                new_flows_and_cellprob_output_final.append(flows_and_cellprob_output_final[image_idx])
 
         # Convert the lists to PyTorch tensors
-        tiled_images_final = torch.stack(new_tiled_images_final)
-        intermediate_outputs_final = torch.stack(new_intermediate_outputs_final)
-        flows_and_cellprob_output_final = torch.stack(new_flows_and_cellprob_output_final)
+        tiled_images_final, intermediate_outputs_final, flows_and_cellprob_output_final = rotation_augmentation(tiled_images_final, intermediate_outputs_final, flows_and_cellprob_output_final)
+        tiled_images_final, intermediate_outputs_final, flows_and_cellprob_output_final = brightness_augmentation(tiled_images_final, intermediate_outputs_final, flows_and_cellprob_output_final)
 
     train_images_tiled, val_images_tiled, train_upsamples, val_upsamples, train_ys, val_ys = train_test_split(
         tiled_images_final, intermediate_outputs_final, flows_and_cellprob_output_final,
